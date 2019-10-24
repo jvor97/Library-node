daa72302172ae7182cf1cba7b2cf9cfa57d2e726
Vorcakova <julia.vorcakova@accenture.com>
2019-10-24 09:17:00 +0200

    chalk

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const chalk = require('chalk');
+
 const app = express();
+
 
 app.get('/', (req,res) => {
     res.send('Hello from my librabry')
 })
 
 app.listen(3000, () => {
-    console.log('listening on port 3000')
+    console.log(`listening on port 3000 ${chalk.green('3000')}`);
 })
