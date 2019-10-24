ea569baed750a71f42b7f04a14198f2db5b1b175
Vorcakova <julia.vorcakova@accenture.com>
2019-10-24 10:02:59 +0200

    sending html file to page

--- app.js
+++ app.js
@@ -2,15 +2,16 @@ const express = require('express');
 const chalk = require('chalk');
 const debug = require('debug')('app');
 const morgan = require('morgan');
+const path = require('path');
 
 const app = express();
 app.use(morgan('tiny'));
 
 
 app.get('/', (req,res) => {
-    res.send('Hello from my librabry')
+    res.sendFile(path.join(__dirname,'/views/index.html'));
 })
 
 app.listen(3000, () => {
-    debug(`listening on port 3000 ${chalk.green('3000')}`);
+    debug(`listening on port ${chalk.green('3000')}`);
 })
