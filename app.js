8fbb21de73fe7c5d67fef4a3f0548513445864a1
Vorcakova <julia.vorcakova@accenture.com>
2019-10-24 09:46:39 +0200

    morgan

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const chalk = require('chalk');
 const debug = require('debug')('app');
+const morgan = require('morgan');
 
 const app = express();
+app.use(morgan('tiny'));
 
 
 app.get('/', (req,res) => {
