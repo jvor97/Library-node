794b2b61e5b69b50cc182580cea856356e952dce
Vorcakova <julia.vorcakova@accenture.com>
2019-10-28 11:38:32 +0100

    get one book with generated id

--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -1,49 +1,10 @@
 const express = require("express");
 const debug = require("debug")("app:bookRoutes");
-const { MongoClient } = require("mongodb");
+const { MongoClient, ObjectID } = require("mongodb");
 
 const bookRouter = express.Router();
 
 function router(nav) {
-  const books = [
-    {
-      author: "Chinua Achebe",
-      pages: 209,
-      title: "Things Fall Apart",
-      year: 1958
-    },
-    {
-      author: "Hans Christian Andersen",
-      pages: 784,
-      title: "Fairy tales",
-      year: 1836
-    },
-    {
-      author: "Dante Alighieri",
-      pages: 928,
-      title: "The Divine Comedy",
-      year: 1315
-    },
-    {
-      author: "Unknown",
-      pages: 160,
-      title: "The Epic Of Gilgamesh",
-      year: -1700
-    },
-    {
-      author: "Unknown",
-      pages: 176,
-      title: "The Book Of Job",
-      year: -600
-    },
-    {
-      author: "Unknown",
-      pages: 288,
-      title: "One Thousand and One Nights",
-      year: 1200
-    }
-  ];
-
   bookRouter.route("/").get((req, res) => {
     const url = "mongodb://localhost:27017";
     const dbName = "LibraryApp";
@@ -73,11 +34,29 @@ function router(nav) {
   bookRouter.route("/:id").get((req, res) => {
     const { id } = req.params;
     //to iste ako const id = req.params.id
-    res.render("singleBook", {
-      nav,
-      title: "Library",
-      book: books[id]
-    });
+    const url = "mongodb://localhost:27017";
+    const dbName = "LibraryApp";
+
+    (async function mongo() {
+      let client;
+      try {
+        client = await MongoClient.connect(url);
+
+        const db = client.db(dbName);
+
+        const col = await db.collection("books");
+        const book = await col.findOne({ _id: new ObjectID(id) });
+        // debug(book);
+        res.render("singleBook", {
+          nav,
+          title: "Library",
+          book
+        });
+      } catch (err) {
+        debug(err.stack);
+      }
+      client.close();
+    })();
   });
   return bookRouter;
 }
