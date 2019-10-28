37e4e4418e569a5291f25aef73bbd862641bdee9
Vorcakova <julia.vorcakova@accenture.com>
2019-10-28 09:52:17 +0100

    connecting to MongoDB

--- app.js
+++ app.js
@@ -29,7 +29,9 @@ const nav = [
   { link: "/authors", title: "Authors" }
 ];
 const bookRouter = require("./src/routes/bookRoutes")(nav);
+const adminRouter = require("./src/routes/adminRoutes")(nav);
 app.use("/books", bookRouter);
+app.use("/admin", adminRouter);
 
 app.get("/", (req, res) => {
   // res.sendFile(path.join(__dirname,'/views/index.html'));
---
+++ src/routes/adminRoutes.js
@@ -0,0 +1,68 @@
+const express = require("express");
+const debug = require("debug");
+const { MongoClient } = require("mongodb");
+
+const adminRouter = express.Router();
+const books = [
+  {
+    author: "Chinua Achebe",
+    pages: 209,
+    title: "Things Fall Apart",
+    year: 1958
+  },
+  {
+    author: "Hans Christian Andersen",
+    pages: 784,
+    title: "Fairy tales",
+    year: 1836
+  },
+  {
+    author: "Dante Alighieri",
+    pages: 928,
+    title: "The Divine Comedy",
+    year: 1315
+  },
+  {
+    author: "Unknown",
+    pages: 160,
+    title: "The Epic Of Gilgamesh",
+    year: -1700
+  },
+  {
+    author: "Unknown",
+    pages: 176,
+    title: "The Book Of Job",
+    year: -600
+  },
+  {
+    author: "Unknown",
+    pages: 288,
+    title: "One Thousand and One Nights",
+    year: 1200
+  }
+];
+
+function router(nav) {
+  adminRouter.route("/").get((req, res) => {
+    const url = "mongodb://localhost:27017";
+    const dbName = "LibraryApp";
+
+    (async function mongo() {
+      let client;
+      try {
+        client = await MongoClient.connect(url);
+        debug("Connected to server");
+
+        const db = client.db(dbName);
+
+        const response = await db.collection("books").insertMany(books);
+        res.json(response);
+      } catch (error) {
+        debug(error.stack);
+      }
+    });
+  });
+  return adminRouter;
+}
+
+module.exports = router;
