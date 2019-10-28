d2b916979cf2e1954aed83e6af97d189e38eb9c6
Vorcakova <julia.vorcakova@accenture.com>
2019-10-28 13:34:58 +0100

    auth routes-getting data from form there

--- app.js
+++ app.js
@@ -3,10 +3,13 @@ const chalk = require("chalk");
 const debug = require("debug")("app");
 const morgan = require("morgan");
 const path = require("path");
+const bodyParser = require("body-parser");
 
 const app = express();
 
 app.use(morgan("tiny"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "/public")));
 app.use(
   "/css",
@@ -30,8 +33,10 @@ const nav = [
 ];
 const bookRouter = require("./src/routes/bookRoutes")(nav);
 const adminRouter = require("./src/routes/adminRoutes")(nav);
+const authRouter = require("./src/routes/authRoutes")(nav);
 app.use("/books", bookRouter);
 app.use("/admin", adminRouter);
+app.use("/auth", authRouter);
 
 app.get("/", (req, res) => {
   // res.sendFile(path.join(__dirname,'/views/index.html'));
